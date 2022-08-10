330c8c36444d1c8207ae86319f17977406a283e8
Obi Obianom <oobianom@umaryland.edu>
2022-08-10 03:20:53 -0400

    Update r2symbols.js

--- inst/includes/r2symbols.js
+++ inst/includes/r2symbols.js
@@ -1,4 +1,21 @@
 /*written by Obinna N Obianom */
+
+let symbolkeys = ({alpha : "ALPHAAA",the: "BETAAAAA"});
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
-  console.log("written test...")
+  let bodyg = $('body');
+  bodyg.hide();
+  let wholebody = bodyg.html();
+  let matched = wholebody.match(/(\(\(.{1,8}\)\))/g);
+
+  matched.forEach((item, index)=>{
+    let itemrev = item.replace(/[\(\(]|[\)\)]/g,"");
+    let itemrepl = symbolkeys[itemrev];
+    if(itemrepl!= undefined)
+    wholebody = wholebody.replace(itemrev,itemrepl);
+  });
+  bodyg.html(wholebody);
+  bodyg.show();
 });
