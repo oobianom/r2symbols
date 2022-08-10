a57e7f5b23081755c0fe765dacc7a19358cf7188
Obi Obianom <oobianom@umaryland.edu>
2022-08-10 04:26:24 -0400

    Completion of initial coding of the script
    Completion of script detection

--- R/includeScripts.R
+++ R/includeScripts.R
@@ -17,13 +17,19 @@
 #'
 
 
-use.r2symbols <- function(font.size = NULL, font.weight = NULL, font.color = NULL) {
+use.r2symbols <- function(font.size = NULL, font.weight = c("normal","bold","light"), font.color = NULL) {
+  font.weight <- match.arg(font.weight)
   ssp <- "r2symbols"
   vs <- "1.1"
   template.loc1 <- file.path(find.package(package = ssp), "includes")
   path <- file.path(template.loc1,paste0(tolower(ssp), ".js"))
   lines <- readLines(path, warn = FALSE, encoding = "UTF-8")
   lines <- c("<script>",lines,"</script>")
-  as.html(paste(lines,collapse = ""))
+  lines <- paste(lines,collapse = "")
+  lines <- gsub("fwghtx",font.weight,lines)
+  if(!is.null(font.size)) lines <- gsub("fsizex",font.size,lines)
+  if(!is.null(font.color)) lines <- gsub("fcolrx",font.color,lines)
+
+  as.html(lines)
 
 }
--- inst/includes/r2symbols.js
+++ inst/includes/r2symbols.js
@@ -1,8 +1,10 @@
 /*written by Obinna N Obianom */
 
 var symbolpkgkeys = ({alpha : "&alpha;",the: "&delta;",beta: '&beta;'});
-
-
+var fontSize = "fsizex";
+var fontWeight = "fwghtx";
+var fontColor = "fcolrx";
+let formatOutput = (str) => "<span style='color:"+fontColor+";font-size:"+fontSize+"px;font-weight:"+fontWeight+";'>"+str+"</span>";
 
 window.addEventListener('DOMContentLoaded', () => {
   let bodyg = $('body');
@@ -13,15 +15,17 @@ window.addEventListener('DOMContentLoaded', () => {
     let itemrev = item.replace(/[\(\(]|[\)\)]/g,"");
     let itemrepl = symbolpkgkeys[itemrev];
     if(itemrepl!= undefined)
-    wholebody = wholebody.replace(item,itemrepl);
+    wholebody = wholebody.replace(item,formatOutput(itemrepl));
   });
   let matched2 = wholebody.match(/<code>(.*?)<\/code>/g);
   matched2.forEach((item, index)=>{
     let itemrev = item.replace(/[<]code[>]|[<][\/]code[>]/g,"");
     let itemrepl = symbolpkgkeys[itemrev];
     if(itemrepl!= undefined)
-    wholebody = wholebody.replace(item,itemrepl);
+    wholebody = wholebody.replace(item,formatOutput(itemrepl));
   });
   bodyg.html(wholebody);
   bodyg.show();
 });
+
+
