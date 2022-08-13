6a29494129af8292dba63afbe17e23b9d5d6b21c
Obi Obianom <oobianom@umaryland.edu>
2022-08-13 04:30:33 -0400

    finalizing scripts
    finalize scripts for modifier

--- R/includeScripts.R
+++ R/includeScripts.R
@@ -18,18 +18,22 @@
 
 
 use.r2symbols <- function(font.size = NULL, font.weight = c("normal","bold","light"), font.color = NULL) {
-  font.weight <- match.arg(font.weight)
+
+  font.weight <- match.arg(font.weight) #set font weight
+  symload(font.size,font.weight,font.color) #init symbols
+
+  #fetch js modifier
   ssp <- "r2symbols"
-  vs <- "1.1"
-  template.loc1 <- file.path(find.package(package = ssp), "includes")
-  path <- file.path(template.loc1,paste0(tolower(ssp), ".js"))
+  path <- system.file(paste0("includes/",ssp, ".js"), package = ssp)
   lines <- readLines(path, warn = FALSE, encoding = "UTF-8")
-  lines <- c("<script>",lines,"</script>")
+  #lines <- c("<script>",lines,"</script>")
   lines <- paste(lines,collapse = "")
   lines <- gsub("fwghtx",font.weight,lines)
   if(!is.null(font.size)) lines <- gsub("fsizex",font.size,lines)
   if(!is.null(font.color)) lines <- gsub("fcolrx",font.color,lines)
 
+  #combine js modifier with symbols and output
+  symbolout <- jsonlite::toJSON(.symbols)
+  lines <- paste(c("<script>var symbolpkgkeys = ",symbolout,";",lines,"</script>"),collapse = "")
   as.html(lines)
-
 }
--- inst/includes/r2symbols.js
+++ inst/includes/r2symbols.js
@@ -1,31 +1,61 @@
 /*written by Obinna N Obianom */
-
-var symbolpkgkeys = ({alpha : "&alpha;",the: "&delta;",beta: '&beta;'});
 var fontSize = "fsizex";
 var fontWeight = "fwghtx";
 var fontColor = "fcolrx";
-let formatOutput = (str) => "<span style='color:"+fontColor+";font-size:"+fontSize+"px;font-weight:"+fontWeight+";'>"+str+"</span>";
+var formatOutput = (str) => "<span style='color:" + fontColor + ";font-size:" + fontSize + "px;font-weight:" + fontWeight + ";'>" + str.htm + "</span>";
+var fetchKeyMatch = (str) => symbolpkgkeys.find(el => el.alias2 == str);
+var fetchAllIndexes = (arr, val = "))") => {
+    var indexes = [],
+        i = -1;
+    while ((i = arr.indexOf(val, i + 1)) != -1) {
+        indexes.push(i);
+    }
+    return indexes.length;
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-  let bodyg = $('body');
-  bodyg.hide();
-  let wholebody = bodyg.html();
-  let matched = wholebody.match(/(\(\(.{1,8}\)\))/g);
-  matched.forEach((item, index)=>{
-    let itemrev = item.replace(/[\(\(]|[\)\)]/g,"");
-    let itemrepl = symbolpkgkeys[itemrev];
-    if(itemrepl!= undefined)
-    wholebody = wholebody.replace(item,formatOutput(itemrepl));
-  });
-  let matched2 = wholebody.match(/<code>(.*?)<\/code>/g);
-  matched2.forEach((item, index)=>{
-    let itemrev = item.replace(/[<]code[>]|[<][\/]code[>]/g,"");
-    let itemrepl = symbolpkgkeys[itemrev];
-    if(itemrepl!= undefined)
-    wholebody = wholebody.replace(item,formatOutput(itemrepl));
-  });
-  bodyg.html(wholebody);
-  bodyg.show();
+    let bodyg = $('body');
+    bodyg.hide();
+    let wholebody = bodyg.html();
+    let matched2 = matched = wholebody.match(/(\(\(.{1,12}\)\))/g);
+    matched.forEach((item, index) => {
+
+        let itemrev = itemrepl = fAI = "";
+
+        fAI = fetchAllIndexes(item);
+
+        if (fAI > 1) {
+
+            item.split("))").forEach((item0, index0) => {
+              console.log("searching -- " + item0);
+        console.log(item0.indexOf("(("));
+                if (item0.indexOf("((") >= 0) {
+                    itemrev = item0.replace(/[\(\(]|[\)\)]/g, "");
+                    itemrepl = fetchKeyMatch(itemrev);
+                    console.log("fetching -- " + itemrev);
+                    if (itemrepl != undefined)
+                        wholebody = wholebody.replace("((" + itemrev + "))", formatOutput(itemrepl));
+                }
+            });
+
+        } else {
+            itemrev = item.replace(/[\(\(]|[\)\)]/g, "");
+            itemrepl = fetchKeyMatch(itemrev);
+            if (itemrepl != undefined)
+                wholebody = wholebody.replace(item, formatOutput(itemrepl));
+        }
+
+
+
+    });
+    matched2 = wholebody.match(/<code>(.*?)<\/code>/g);
+    matched2.forEach((item, index) => {
+        let itemrev = itemrepl = "";
+        itemrev = item.replace(/[<]code[>]|[<][\/]code[>]/g, "");
+        itemrepl = fetchKeyMatch(itemrev);
+        if (itemrepl != undefined)
+            wholebody = wholebody.replace(item, formatOutput(itemrepl));
+    });
+    bodyg.html(wholebody);
+    bodyg.show();
 });
-
-
