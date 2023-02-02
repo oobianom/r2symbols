ab1efcc201c4f6f8d760a2d1bb5402ab397f5da1
Obi Obianom <oobianom@umaryland.edu>
2023-02-02 03:07:09 -0500

    Create fetchCat.R

---
+++ R/fetchCat.R
@@ -0,0 +1,30 @@
+#' Fetch symbols by category
+#'
+#' Fetch one or more symbols by category
+#'
+#' @param category symbol categories to fetch
+#' @param font.size font size as integer
+#' @param font.weight font weight as integer
+#' @param font.color font color e.g. red, blue, cyan
+#' @param if.error error response
+#' @param search.units.each whether to search single letter columns
+#'
+#' @section Complete cheatcheat for symbols:
+#' Common symbols include alpha, beta, copyright, registered, theta, sigma. See the complete list at
+#' \url{https://github.com/oobianom/r2symbols}.
+#'
+#' @return get symbols from a particular category
+#'
+#' @examples
+#' symCategory("arrows")
+#' symCategory("smiley")
+#' symCategory("music")
+#' symCategory("random")
+#'
+#' @export
+
+symCategory <- function(category, font.size = NULL, font.weight = NULL, font.color = NULL, if.error = FALSE, search.units.each = FALSE) {
+
+# codes
+
+}
