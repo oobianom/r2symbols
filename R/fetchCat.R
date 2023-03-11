9ecbd8f1a917f2c598cece608b35891ba46012db
Obi Obianom <oobianom@umaryland.edu>
2023-03-11 13:02:05 -0500

    Update fetchCat.R

--- R/fetchCat.R
+++ R/fetchCat.R
@@ -1,8 +1,8 @@
-#' Fetch symbols by category
+#' Fetch multiple symbols by category
 #'
 #' Fetch one or more symbols by category
 #'
-#' @param category symbol categories to fetch
+#' @param category symbol categories to fetch (minimum: 4 characters)
 #' @param font.size font size as integer
 #' @param font.weight font weight as integer
 #' @param font.color font color e.g. red, blue, cyan
@@ -16,15 +16,65 @@
 #' @return get symbols from a particular category
 #'
 #' @examples
-#' symCategory("arrows")
-#' symCategory("smiley")
-#' symCategory("music")
-#' symCategory("random")
+#' symCat("arrows")
+#' symCat("smiley")
+#' symCat("music")
+#' symCat("random")
 #'
 #' @export
 
-symCategory <- function(category, font.size = NULL, font.weight = NULL, font.color = NULL, if.error = FALSE, search.units.each = FALSE) {
+symCat <- function(category, font.size = NULL, font.weight = NULL, font.color = NULL, if.error = FALSE, search.units.each = FALSE) {
 
-# codes
+  # remove non-alphnumeric
+  category <- gsub("[^a-zA-Z]","",category)
+
+  # exit if category is less than 4 characters
+  if(nchar(category) < 4) stop("Provide at atleast 4 characters for the category")
+
+  # load symbols if not loaded
+  if(!exists('.symbols_ph'))symload(font.size = font.size, font.weight = font.weight, font.color = font.color)
+
+  # select symbols by category
+  sym.list <- .symbols[grep(category,.symbols$category,ignore.case = TRUE) & (!is.na(.symbols$alias)),]$alias
+
+  # return error if no symbols are picked, else return symbols
+  if(length(sym.list)){
+    symbol(sym.list)
+  }else{
+    warning("No symbols match the category term that was entered")
+  }
+
+}
+
+
+
+#' Fetch multiple symbols by keyword
+#'
+#' Fetch one or more symbols by keyword
+#'
+#' @param Keyword symbol keyword to fetch
+#' @param font.size font size as integer
+#' @param font.weight font weight as integer
+#' @param font.color font color e.g. red, blue, cyan
+#' @param if.error error response
+#' @param search.units.each whether to search single letter columns
+#'
+#' @section Complete cheatcheat for symbols:
+#' Common symbols include alpha, beta, copyright, registered, theta, sigma. See the complete list at
+#' \url{https://github.com/oobianom/r2symbols}.
+#'
+#' @return get symbols from a particular category
+#'
+#' @examples
+#' symKey("arrows")
+#' symKey("smiley")
+#' symKey("music")
+#' symKey("random")
+#'
+#' @export
+
+symKey <- function(Keyword, font.size = NULL, font.weight = NULL, font.color = NULL, if.error = FALSE, search.units.each = FALSE) {
+
+  # codes
 
 }
