de2b26cbffcecef92eeb2994848e5b3e9054fdc3
Obi Obianom <oobianom@umaryland.edu>
2023-03-11 18:37:05 -0500

    Published CRAN 1.4

---
+++ vignettes/fetching_multiple_symbols_r.R
@@ -0,0 +1,22 @@
+## ----eval=FALSE---------------------------------------------------------------
+#  library(r2symbols)
+
+## -----------------------------------------------------------------------------
+# fetch a group of symbols by using keywords
+symKey(keyword = "chess", font.size = 40)
+
+# when the keyword doesn't exist
+symKey(keyword = "chf34ess", font.size = 40)
+
+## ----eval=FALSE---------------------------------------------------------------
+#  # when the keyword is not up to 4 characters
+#  symKey(keyword = "chf", font.size = 40) # retruns error
+
+## -----------------------------------------------------------------------------
+# fetch a group of symbols by using category words
+# categories include: arrows mixed chess check mark symbol animal emoji flag people skull sign star telephone weather zodiac smiley hourglass fraction cards triangle square
+symCat(category = "arrow", font.color="blue", font.size = 20)
+symCat(category = "people", font.color="gray", font.weight = "light",font.size = 50)
+symCat(category = "emoji", font.color="gray", font.weight = "light",font.size = 22)
+symCat(category = "smiley", font.color="gray", font.weight = "light",font.size = 32)
+
---
+++ vignettes/introduction_r_symbols.R
@@ -0,0 +1,56 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+library(r2symbols)
+symbol("alpha")
+symbol("beta")
+symbol("mouse")
+symbol(456)
+
+# OR together
+symbol("alpha","beta")
+
+# OR use alias
+sym("alpha","beta")
+
+## -----------------------------------------------------------------------------
+library(r2symbols)
+symbol.setting(font.size=50,font.weight ="normal",font.color = "blue")
+symbol("copyright","heart","chessq","ox","deer","beer")
+
+# OR use alias
+sym("naira","yen","music1")
+
+# change settings anytime
+symbol.setting(font.size=30,font.weight ="bold",font.color = "red")
+sym("naira","yen")
+sym("arrow-left","arrow-up")
+
+# Remove global settings
+symbol.setting()#leaving the arguments empty means they are NULL
+symbol("indian","naira")
+
+# OR use alias
+sym("minusplus","infinity")
+
+
+## -----------------------------------------------------------------------------
+library(r2symbols)
+symbol("minusplus",font.size=40,font.weight ="normal",font.color = "green")
+
+# OR use alias
+symbol("integral",font.size=30,font.weight ="bold",font.color = "blue")
+
+#multiple symbols and settings
+sym("naira","yen","minusplus",font.color="violet")
+
+
+## -----------------------------------------------------------------------------
+library(r2symbols)
+symbol.setting(font.size=30,font.weight ="bold",font.color = "brown")
+
+
